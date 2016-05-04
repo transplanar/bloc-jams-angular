6430c0f9e7be1296d399994297d86d1b9fe041ff
transplanar <transplanar@gmail.com>
2016-05-04 15:22:15 -0400

    Pause playback functionality for SongPlayer service

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,18 +1,54 @@
 (function(){
   function SongPlayer() {
     var SongPlayer = {};
+
+    var currentSong = null;
     
-    SongPlayer.play = function(song) {
-      console.log('attempting to play song');
-      var currentBuzzObject = new buzz.sound(song.audioUrl, {
+    /**
+     * @desc Buzz object audio file
+     * @type {Object}
+   */
+    var currentBuzzObject = null;
+    
+    /**
+     * @function setSong
+     * @desc Stops currently playing song and loads selected song as currentBuzzObject
+     * @param {Object} song
+   */
+    var setSong = function(song){
+      if (currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = false; //NOTE should this be null? Why?
+      }
+      
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
       });
+      
+      currentSong = song;
+    };
 
-      currentBuzzObject.play();    
-      console.log('playing song');
+    //TODO missing resume play of current song
+    SongPlayer.play = function(song) {
+      if(currentSong !== song){
+        setSong(song);  
+        currentBuzzObject.play();
+        
+        song.playing = true;
+        
+      } else if (currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+        }
+      }      
     };
     
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+    };
+
     return SongPlayer;
   }
   
