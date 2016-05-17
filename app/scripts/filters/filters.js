74855d091fb5142e5a08aac4c15c9ad9e336eafb
transplanar <transplanar@gmail.com>
2016-05-17 14:04:29 -0400

    Refactor time format filter to use buzz library toTimer function`

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -3,15 +3,8 @@ angular.module('formatTime',[])
     return function(time){
       if( Number.isNaN(Number.parseFloat(time)) ){
         return '-:--';
+      }else{
+        return buzz.toTimer(time);
       }
-      
-      var minutes = Math.floor(time / 60);
-      var seconds  = Math.floor(time % 60);
-
-      if(seconds < 10){
-        seconds = '0' + seconds;
-      }
-
-      return minutes + ":" + seconds;
     }
   }]);
